70c2d9cf3abdbf9d445869d2919f36e8b2c6455d
Diego Ferreira Alves <diego.alves@ivoryit.com.br>
2020-02-19 11:03:52 -0300

    Armazenamento de objetos em local storage

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -1,69 +1,156 @@
-import React from 'react';
+import React from "react";
+import ProdutoService from "../../app/produtoService";
+
+const estadoInicial = {
+  nome: "",
+  sku: "",
+  descricao: "",
+  preco: 0,
+  fornecedor: "",
+  //
+  sucesso: false,
+};
 
 class CadastroProduto extends React.Component {
-    render() {
-        return(
-            <div className="card">
-                <div className="card-header">
-                    Cadastro de Produto
-               </div>
-               <div className="card-body">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <label>Nome: *</label>
-                                <input type="text" className="form-control"/>
-                            </div>
-                        </div>
+  state = estadoInicial;
 
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <label>SKU: *</label>
-                                <input type="text" className="form-control"/>
-                            </div>
-                        </div>
-                    </div>
+  constructor() {
+    super();
+    this.service = new ProdutoService();
+  }
 
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="form-group">
-                                <labe>Descrição: </labe>
-                                <textarea className="form-control"/>
-                            </div>
-                        </div>
-                    </div>
+  onChangeHandler = event => {
+    const valorDoCampo = event.target.value;
+    const nomeDoCampo = event.target.name;
 
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <labe>Preço: *</labe>
-                                <input type="text" className="form-control"/>
-                            </div>
-                        </div>
+    this.setState({
+      [nomeDoCampo]: valorDoCampo
+    });
+  };
 
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <labe>Fornecedor: *</labe>
-                                <input type="text" className="form-control"/>
-                            </div>
-                        </div>
-                    </div>
+  onSubmitHandler = event => {
+    const produto = {
+      nome: this.state.nome,
+      sku: this.state.sku,
+      descricao: this.state.descricao,
+      preco: this.state.preco,
+      fornecedor: this.state.fornecedor
+    };
 
-                    <div className="row">
-                        <div className="col-md-1">
-                            <button className="btn btn-success">Salvar</button>
-                        </div>
+    this.service.salvar(produto);
+    this.limpaCampos();
+    console.log("Salvo com Sucesso");
+  };
 
-                        <div className="col-md-1">
-                            <button className="btn btn-primary">Limpar</button>
-                        </div>
-                    </div>
+  limpaCampos = () => {
+    this.setState(estadoInicial);
+  };
 
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">Cadastro de Produto</div>
+        <div className="card-body">
 
-               </div>
+          {this.state.sucesso ? (
+              <div class="alert alert-dismissible alert-success">
+              <button type="button" class="close" data-dismiss="alert">
+                &times;
+              </button>
+              <strong>Sucesso!</strong> O produto foi cadastrado corretamente.
             </div>
-        );
-    }
+          ) : null }
+
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Nome: *</label>
+                <input
+                  type="text"
+                  name="nome"
+                  onChange={this.onChangeHandler}
+                  value={this.state.nome}
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>SKU: *</label>
+                <input
+                  type="text"
+                  name="sku"
+                  onChange={this.onChangeHandler}
+                  value={this.state.sku}
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>Descrição: </label>
+                <textarea
+                  name="descricao"
+                  onChange={this.onChangeHandler}
+                  value={this.state.descricao}
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Preço: *</label>
+                <input
+                  type="text"
+                  name="preco"
+                  onChange={this.onChangeHandler}
+                  value={this.state.preco}
+                  className="form-control"
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Fornecedor: *</label>
+                <input
+                  type="text"
+                  name="fornecedor"
+                  onChange={this.onChangeHandler}
+                  value={this.state.fornecedor}
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-1">
+              <button
+                className="btn btn-success"
+                onClick={this.onSubmitHandler}
+              >
+                Salvar
+              </button>
+            </div>
+
+            <div className="col-md-1">
+              <button className="btn btn-primary" onClick={this.limpaCampos}>
+                Limpar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CadastroProduto;
