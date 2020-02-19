3a3919b18663996327b9a6ffeaa96ab40a3729dc
Diego Ferreira Alves <diego.alves@ivoryit.com.br>
2020-02-19 09:08:41 -0300

    Nova paginação e componentes

---
+++ src/views/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div class="jumbotron">
+      <h1 class="display-3">Bem Vindo!</h1>
+      <p class="lead">
+        Este é seu sistema, utilize a barra de navegação para acessar as páginas.
+      </p>
+      <hr class="my-4" />
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">
+          Cadastrar
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
